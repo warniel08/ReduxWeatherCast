32dc1bc9a65540f9fe4b1f44bf639ec2dd060cab
Warner Nielsen <bwn888@gmail.com>
2017-07-21 17:48:27 -0600

    Update package.json with axios, make ajax request with axios in actions/index.js file

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${process.env.APPID_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: 'FETCH_WEATHER',
+    payload: request
+  };
+}
