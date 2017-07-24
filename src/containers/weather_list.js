8ba56437c193dd4646375443cb45330937db1ae2
Warner Nielsen <bwn888@gmail.com>
2017-07-23 18:03:12 -0600

    Create google map component, style components correctly

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
-
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const city_name = cityData.city.name;
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => ((temp * 9/5) - 459.67));
-    console.log(temps);
     const pressures = _.map(cityData.list.map(weather => weather.main.pressure), (pressure) => (pressure * 0.02952998751));
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return (
       <tr key={ city_name }>
-        <td>{ city_name }</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color='orange' units='F' /></td>
         <td><Chart data={pressures} color='blue' units='inHg' /></td>
         <td><Chart data={humidities} color='green' units='%' /></td>
