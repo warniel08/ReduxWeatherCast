559ba2869f2c449e2300e032a6fecdc27329a74f
Warner Nielsen <bwn888@gmail.com>
2017-07-25 10:44:50 -0600

    Update api key to load in index.html

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import ReduxPromise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
+import API_KEY from './keys';
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
